14c4e349870ef0b48d67bf69477b6c248646e00c
kartikprabhu20 <kartikprabhu20@yahoo.co.in>
2020-01-28 19:07:25 +0100

    #initial commit

---
+++ 2_tensorBoard/EPIC_DEEP_LEARNING_FAIL_COMPILATION_OCTOBER_2019/fail3.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+
+from datasets import MNISTDataset
+from time import time
+
+
+# get the data
+(train_imgs, train_lbls), (test_imgs, test_lbls) = tf.keras.datasets.mnist.load_data()
+mnist = MNISTDataset(train_imgs.reshape((-1, 784)), train_lbls,
+                     test_imgs.reshape((-1, 784)), test_lbls,
+                     batch_size=256, seed=int(time()))
+
+
+# define the model first, from input to output
+
+# let's use fewer layers...
+n_units = 100
+n_layers = 2
+
+# just set up a "chain" of hidden layers
+layers = []
+for layer in range(n_layers):
+    layers.append(tf.keras.layers.Dense(
+        n_units, activation=tf.nn.relu,
+        kernel_initializer=tf.initializers.RandomUniform(minval=-0.01,
+                                                         maxval=0.),
+        bias_initializer=tf.initializers.constant(0.001)))
+
+# finally add the output layer
+layers.append(tf.keras.layers.Dense(
+    10, kernel_initializer=tf.initializers.RandomUniform(minval=-0.01,
+                                                         maxval=0.01)))
+
+lr = 0.1
+for step in range(2000):
+    img_batch, lbl_batch = mnist.next_batch()
+    with tf.GradientTape() as tape:
+        # here we just run all the layers in sequence via a for-loop
+        out = img_batch
+        for layer in layers:
+            out = layer(out)
+        xent = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
+            logits=out, labels=lbl_batch))
+
+    weights = [var for l in layers for var in l.trainable_variables]
+    grads = tape.gradient(xent, weights)
+    for grad, var in zip(grads, weights):
+        var.assign_sub(lr*grad)
+
+    if not step % 100:
+        preds = tf.argmax(out, axis=1, output_type=tf.int32)
+        acc = tf.reduce_mean(tf.cast(tf.equal(preds, lbl_batch), tf.float32))
+        print("Loss: {} Accuracy: {}".format(xent, acc))
+
+
+out = mnist.test_data
+for layer in layers:
+    out = layer(out)
+test_preds = tf.argmax(out, axis=1, output_type=tf.int32)
+acc = tf.reduce_mean(tf.cast(tf.equal(test_preds, mnist.test_labels), tf.float32))
+print("Final test accuracy: {}".format(acc))
